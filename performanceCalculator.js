13c4da159cf3a6782793d89bc3ba3260418be350
Gregory Shoup <gregshoup@gmail.com>
2022-11-02 18:39:57 -0400

    basic web pages created including resume and bio

---
+++ performanceCalculator.js
@@ -0,0 +1,7 @@
+const samplePortfolio = [
+    {stock: 'AAPL', quantity: 1000},
+    {stock: 'ITW', quantity: 500},
+    {stock: 'SHOP', quantity: 200},
+    {stock: 'TROW', quantity: 750},
+    {stock: 'ABNB', quantity: 2000},
+]
